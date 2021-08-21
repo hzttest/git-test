03ebf7955693804ae46625cffe7d51ee110ebde2
hzt <2530165152@qq.com>
2021-08-22 01:14:11 +0800

    first commit

---
+++ src/main/java/com/example/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.example.git;
+
+/**
+ * @author hzt
+ * @create 2021-08-22 1:10
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
